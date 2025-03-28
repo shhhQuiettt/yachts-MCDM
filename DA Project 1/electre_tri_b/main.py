0967b2f0309b60a0739f33cfc327e409d9b9677a
shhhQuiettt <krzysztof.skro@gmail.com>
2025-03-28 15:04:34 +0100

    Develop TRI-B

--- DA Project 1/electre_tri_b/main.py
+++ DA Project 1/electre_tri_b/main.py
@@ -2,9 +2,10 @@ from pathlib import Path
 
 import click
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 import pandas as pd
 
-from electre_tri_b.utils import (
+from utils import (
     load_dataset,
     load_boundary_profiles,
     load_indifference_thresholds,
@@ -16,10 +17,10 @@ from electre_tri_b.utils import (
 
 
 # TODO
-def calculate_marginal_concordance_index[
-    T: (float, np.ndarray),
-    U: (float, np.ndarray),
-](diff: T, q: U, p: U) -> T:
+def calculate_marginal_concordance_index[T: (
+    float,
+    np.ndarray,
+), U: (float, np.ndarray),](diff: T, q: U, p: U) -> T:
     """
     Function that calculates the marginal concordance index for the given pair of alternatives, according to the formula presented during classes.
 
@@ -28,7 +29,14 @@ def calculate_marginal_concordance_index[
     :param p: preference threshold either as a float if you prefer to calculate for a single criterion or as numpy array for multiple criterion
     :return: marginal concordance index either as a float for single criterion and alternative pairs, or as numpy array for multiple criterion
     """
-    raise NotImplementedError()
+
+    concordance = np.clip(
+        (p + diff) / (p - q),
+        0,
+        1,
+    )
+
+    return concordance
 
 
 # TODO
@@ -49,10 +57,55 @@ def calculate_marginal_concordance_matrix(
     :param criterion_types: pandas dataframe with a column 'type' representing the type of criterion (either gain or cost)
     :return: 4D numpy array with marginal concordance matrix with shape [2, number of alternatives, number of boundary profiles, number of criterion], where element with index [0, i, j, k] describe marginal concordance index between alternative i and boundary profile j on criterion k, while element with index [1, i, j, k] describe marginal concordance index between boundary profile j and  alternative i on criterion k
     """
-    raise NotImplementedError()
+
+    # raw_dataset = dataset.to_numpy()
+    concordances = []
+    for criterion_id in dataset.columns:
+        criterion_concordances = []
+        for boundary_id in boundary_profiles.index:
+            criterion_values = dataset.loc[:, criterion_id].to_numpy()
+            boundary_profile_value = boundary_profiles.at[boundary_id, criterion_id]
+
+            criterion_type = criterion_types.at[criterion_id, "type"]
+            assert criterion_type == "gain" or criterion_type == "cost"
+
+            diff = (
+                criterion_values - boundary_profile_value
+                if criterion_type == "gain"
+                else boundary_profile_value - criterion_values
+            )
+            indifference_threshold = indifference_thresholds.at[
+                boundary_id, criterion_id
+            ]
+            preference_threshold = preference_thresholds.at[boundary_id, criterion_id]
+
+            marginal_concordance_index_alt_to_bound = calculate_marginal_concordance_index(
+                diff,
+                q=indifference_threshold,
+                p=preference_threshold,
+            )
+
+            marginal_concordance_index_bound_to_alt =  calculate_marginal_concordance_index(
+                -diff,
+                q=indifference_threshold,
+                p=preference_threshold,
+            )
+
+            criterion_concordances.append(
+                    np.vstack((marginal_concordance_index_alt_to_bound, marginal_concordance_index_bound_to_alt)),
+                               )
+            # print(criterion_concordances[-1].shape)
+        # print(np.dstack(criterion_concordances).shape)
+        concordances.append(np.dstack(criterion_concordances))
+        assert concordances[-1].shape == (2, len(dataset.index), len(boundary_profiles.index)), f"Expected: {(2, len(dataset.index), len(boundary_profiles.index))} Got: {concordances[-1].shape}"
+
+    concordances_numpy = np.stack(concordances, axis = 3)
+    assert concordances_numpy.shape == (2, len(dataset.index), len(boundary_profiles.index), len(dataset.columns)), f"Expected: {(2, len(dataset.index), len(boundary_profiles.index, len(dataset.columns)))} Got: {concordances_numpy.shape}"
+
+    return concordances_numpy
 
 
-# TODO
+
 def calculate_comprehensive_concordance_matrix(
     marginal_concordance_matrix: np.ndarray, criterion_types: pd.DataFrame
 ) -> np.ndarray:
@@ -63,14 +116,17 @@ def calculate_comprehensive_concordance_matrix(
     :param criterion_types: dataframe that contains "k" column with criterion weights
     :return: 3D numpy array with comprehensive concordance matrix with shape [2, number of alternatives, number of boundary profiles], where element with index [0, i, j] describe comprehensive concordance index between alternative i and boundary profile j, while element with index [1, i, j] describe comprehensive concordance index between boundary profile j and  alternative i
     """
-    raise NotImplementedError()
+    weights = criterion_types.loc[:, "k"].to_numpy()
+    comprehensive_concordance_matrix = np.average(marginal_concordance_matrix, axis = 3, weights = weights)
+    return comprehensive_concordance_matrix
+
 
 
 # TODO
-def calculate_marginal_discordance_index[
-    T: (float, np.ndarray),
-    U: (float, np.ndarray),
-](diff: T, p: U, v: U) -> T:
+def calculate_marginal_discordance_index[T: (
+    float,
+    np.ndarray,
+), U: (float, np.ndarray),](diff: T, p: U, v: U) -> T:
     """
     Function that calculates the marginal concordance index for the given pair of alternatives, according to the formula presented during classes.
 
@@ -79,7 +135,14 @@ def calculate_marginal_discordance_index[
     :param v: veto threshold either as a float if you prefer to calculate for a single criterion or as numpy array for multiple criterion
     :return: marginal discordance index either as a float for single criterion and alternative pairs, or as numpy array for multiple criterion
     """
-    raise NotImplementedError()
+
+    discordance = np.clip(
+            (-diff - p) / (v-p),
+            0,
+            1,
+            )
+
+    return discordance
 
 
 # TODO
@@ -100,7 +163,58 @@ def calculate_marginal_discordance_matrix(
     :param criterion_types: pandas dataframe with a column 'type' representing the type of criterion (either gain or cost)
     :return: 4D numpy array with marginal discordance matrix with shape [2, number of alternatives, number of boundary profiles, number of criterion], where element with index [0, i, j, k] describe marginal discordance index between alternative i and boundary profile j on criterion k, while element with index [1, i, j, k] describe marginal discordance index between boundary profile j and  alternative i on criterion k
     """
-    raise NotImplementedError()
+    discordances = []
+    for criterion_id in dataset.columns:
+        criterion_discordances = []
+        for boundary_id in boundary_profiles.index:
+            veto_threshold = veto_thresholds.at[boundary_id, criterion_id]
+            if np.isnan(veto_threshold):
+                criterion_discordances.append(
+                        np.zeros((2, len(dataset.index)))
+                    )
+                continue
+
+            preference_threshold = preference_thresholds.at[boundary_id, criterion_id]
+
+            criterion_values = dataset.loc[:, criterion_id].to_numpy()
+            boundary_profile_value = boundary_profiles.at[boundary_id, criterion_id]
+
+            criterion_type = criterion_types.at[criterion_id, "type"]
+            assert criterion_type == "gain" or criterion_type == "cost"
+
+            diff = (
+                criterion_values - boundary_profile_value
+                if criterion_type == "gain"
+                else boundary_profile_value - criterion_values
+            )
+
+
+            marginal_discordance_index_alt_to_bound = calculate_marginal_discordance_index(
+                diff,
+                p=preference_threshold,
+                v=veto_threshold
+            )
+
+            marginal_discordance_index_bound_to_alt =  calculate_marginal_discordance_index(
+                -diff,
+                p=preference_threshold,
+                v=veto_threshold
+            )
+
+            criterion_discordances.append(
+                    np.vstack((marginal_discordance_index_alt_to_bound, marginal_discordance_index_bound_to_alt)),
+                               )
+        discordances.append(np.dstack(criterion_discordances))
+        assert discordances[-1].shape == (2, len(dataset.index), len(boundary_profiles.index)), f"Expected: {(2, len(dataset.index), len(boundary_profiles.index))} Got: {discordances[-1].shape}"
+
+    discordances_numpy = np.stack(discordances, axis = 3)
+    assert discordances_numpy.shape == (2, len(dataset.index), len(boundary_profiles.index), len(dataset.columns)), f"Expected: {(2, len(dataset.index), len(boundary_profiles.index, len(dataset.columns)))} Got: {discordances_numpy.shape}"
+
+    print(discordances_numpy[1,:,0,:])
+
+    return discordances_numpy
+
+
 
 
 # TODO
@@ -115,7 +229,14 @@ def calculate_credibility_index(
     :param marginal_discordance_matrix: 3D numpy array with marginal discordance matrix, Consecutive indices [i, j, k] describe first alternative, second alternative, criterion
     :return: 3D numpy array with credibility matrix with shape [2, number of alternatives, number of boundary profiles], where element with index [0, i, j] describe credibility index between alternative i and boundary profile j, while element with index [1, i, j] describe credibility index between boundary profile j and  alternative i
     """
-    raise NotImplementedError()
+    credibility_index = comprehensive_concordance_matrix.copy()
+
+    criterion_number = marginal_discordance_matrix.shape[-1]
+    sufficienly_great_discordance_mask = marginal_discordance_matrix > as_strided(credibility_index, shape = credibility_index.shape + (criterion_number,),
+                                                                                  strides = credibility_index.strides + (0,))
+
+        
+
 
 
 # TODO
@@ -191,6 +312,7 @@ def electre(dataset_path: Path) -> None:
         preference_thresholds,
         criterion_types,
     )
+
     comprehensive_concordance_matrix = calculate_comprehensive_concordance_matrix(
         marginal_concordance_matrix, criterion_types
     )
@@ -206,18 +328,18 @@ def electre(dataset_path: Path) -> None:
     credibility_index = calculate_credibility_index(
         comprehensive_concordance_matrix, marginal_discordance_matrix
     )
-    outranking_relation_matrix = calculate_outranking_relation_matrix(
-        credibility_index, credibility_threshold
-    )
-    relation = calculate_relation(
-        outranking_relation_matrix, dataset.index, boundary_profiles.index
-    )
+    # outranking_relation_matrix = calculate_outranking_relation_matrix(
+    #     credibility_index, credibility_threshold
+    # )
+    # relation = calculate_relation(
+    #     outranking_relation_matrix, dataset.index, boundary_profiles.index
+    # )
 
-    pessimistic_assigment = calculate_pessimistic_assigment(relation)
-    optimistic_assigment = calculate_optimistic_assigment(relation)
+    # pessimistic_assigment = calculate_pessimistic_assigment(relation)
+    # optimistic_assigment = calculate_optimistic_assigment(relation)
 
-    print("pessimistic assigment\n", pessimistic_assigment)
-    print("optimistic assigment\n", optimistic_assigment)
+    # print("pessimistic assigment\n", pessimistic_assigment)
+    # print("optimistic assigment\n", optimistic_assigment)
 
 
 if __name__ == "__main__":
