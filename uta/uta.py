2265500f704de9007030943751a9b88aefaf5e72
wojbog <wojtek44wojciech@gmail.com>
2025-05-06 14:53:43 +0200

    last commit

--- uta/uta.py
+++ uta/uta.py
@@ -1,14 +1,16 @@
 import pandas as pd
 # read csv
 
-df = pd.read_csv("dataset/uta/utility.csv")
+df = pd.read_csv("dataset/ahp/utility.csv")
 
-# print(df.head())
+# sort df according to Utility column
+# df.sort_values(by=["Utility"], ascending=False, inplace=True)
+# print the sorted dataframe
+# print(df)
 
-# set frist column as index
 df.set_index(df.columns[0], inplace=True)
 
-# print(df.head())
+
 
 from promethee.utils import Relation
 complete_ranking = set()
@@ -28,14 +30,40 @@ for i in range(number_of_alternative):
             complete_ranking.add((df.index.tolist()[i], df.index.tolist()[j], Relation.INCOMPARABLE))
 
 from promethee.utils import display_ranking
-# from promethee.main import create_complete_ranking
-# create complete ranking
-# ranking = create_complete_ranking(df)
-# print(ranking)
-
-# print(ranking.iloc[0] > ranking.iloc[1])
-
-display_ranking(complete_ranking, "UTA Method")
 
 
+display_ranking(complete_ranking, "AHP Method")
 
+# from scipy.stats import kendalltau
+
+# # Map item names to IDs
+# names = [
+#     "Laguna 25", "Maxus 24", "Antila 24", "Antila 24.4 #2", "Aquatic 25T", "Janmor 25",
+#     "Antila 24.4", "Mariner 24", "Maxus 24 Evo #2", "Phobos 25", "Antila 28.8",
+#     "Maxus 24 Evo", "Tes 246 Versus", "Phobos 24.5 #2", "Phobos 24.5", "Phobos 25 #2"
+# ]
+# name_to_index = {name: i for i, name in enumerate(names)}
+
+# # Score-based ranking (original table)
+# score_ranking = [
+#     "Laguna 25", "Maxus 24", "Antila 24", "Antila 24.4 #2", "Aquatic 25T", "Janmor 25",
+#     "Antila 24.4", "Mariner 24", "Maxus 24 Evo #2", "Phobos 25", "Antila 28.8",
+#     "Maxus 24 Evo", "Tes 246 Versus", "Phobos 24.5 #2", "Phobos 24.5", "Phobos 25 #2"
+# ]
+# score_ranking_idx = [name_to_index[name] for name in score_ranking]
+
+# # Utility-based ranking (new list)
+# utility_ranking = [
+#     "Laguna 25", "Antila 24.4 #2", "Maxus 24", "Antila 24", "Phobos 24.5", "Phobos 25 #2",
+#     "Janmor 25", "Maxus 24 Evo #2", "Phobos 25", "Mariner 24", "Tes 246 Versus",
+#     "Antila 28.8", "Phobos 24.5 #2", "Antila 24.4", "Aquatic 25T", "Maxus 24 Evo"
+# ]
+# utility_ranking_idx = [name_to_index[name] for name in utility_ranking]
+
+# # Compute Kendall's tau and distance
+# tau, _ = kendalltau(score_ranking_idx, utility_ranking_idx)
+# n = len(score_ranking_idx)
+# kendall_distance = int((1 - tau) * n * (n - 1) / 2)
+
+# print("Kendall's tau:", round(tau, 4))
+# print("Kendall's distance:", kendall_distance)
